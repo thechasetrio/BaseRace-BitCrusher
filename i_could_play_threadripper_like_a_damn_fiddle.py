3459deb9f8064e33a3c97409c4e3a9d06ca6e59e
VincentVaughn <33636265+VincentVaughn@users.noreply.github.com>
2017-12-03 19:32:27 -0800

    Added a Python multithreading thing

---
+++ i_could_play_threadripper_like_a_damn_fiddle.py
@@ -0,0 +1,43 @@
+import pygame, time, random, threading, multiprocessing
+from pygame.locals import *
+
+global window
+
+class blitThread(threading.Thread):
+   def __init__(self, startPoint, dims):
+      threading.Thread.__init__(self)
+      self.startPoint = startPoint
+      self.currentPixel = pygame.Surface([1, 1]).convert()
+      self.dims = dims
+   def run(self):
+      for x in range(self.dims[0]):
+          for y in range(self.dims[1]):
+              self.currentPixel.fill((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+              window.blit(self.currentPixel, [self.startPoint[0] + x, self.startPoint[1] + y])
+              
+
+global window
+
+window = pygame.display.set_mode((1920, 1080), pygame.FULLSCREEN | pygame.HWSURFACE | pygame.DOUBLEBUF)
+
+fuckyou = multiprocessing.cpu_count() - 1
+
+freds = []
+
+for butts in range(fuckyou):
+    freds.append(blitThread([butts * 100, butts * 100], [100, 100]))
+
+for ffff in freds:
+    ffff.start()
+
+while threading.activeCount() > 1:
+    pass
+
+pygame.display.flip()
+
+startTime = time.clock()
+
+while time.clock() < startTime + 5:
+    pass
+
+pygame.quit()
